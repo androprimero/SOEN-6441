c12212c69610a201996402122a6ff4ab3ea2c082
androprimero <andro_polis@hotmail.com>
2017-07-27 20:44:18 -0400

    Application complete
    cheers.py is the main file, its starts the application.

---
+++ FileManager.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Filename: FileManager.py
+Author: Alejandro Bernal
+version: 1.0
+Description:
+Module that has FileManager class, which create, open write and read a text plain file.
+"""
+class FileManager:
+    file_name = ""
+
+    def __init__(self, file_name):
+        if file_name != "":
+            self.file_name = file_name
+            self.file = open(self.file_name, "a+")
+        else:
+            self.file_name = "result.txt"
+            self.file = open(self.file_name, "a+")
+
+    def save(self, data):
+        self.file.write(data)
+
+    def read(self, beginning):
+        if beginning:
+            self.file.seek(0, 0)
+        return self.file.readline()
+
+    def close(self):
+        if not self.file.closed:
+            return self.file.close()
---
+++ cheers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from UserInterfaceModule import UserMainInterface
+"""
+Filename: cheers.py
+Author: Alejandro Bernal
+version: 1.0
+Description:
+Module that has the main method. It starts the user interface.
+"""
+
+def main():
+    user_interface = UserMainInterface()
+    user_interface.start()
+
+if __name__ == "__main__":
+    main()
